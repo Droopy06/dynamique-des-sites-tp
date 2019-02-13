db769c101a1ca343bf336a125786017b9f213a62
Alexandre LAMOOT <lamootalexandre@gmail.com>
2019-02-13 23:40:19 +0100

    feat(ajax): add ajax training

---
+++ ajax/README.md
@@ -0,0 +1,22 @@
+# dynamique-des-sites-tp
+
+## TP Ajax
+
+## Etape 1
+### Effectuer une requête GET avec ajax afin de visualiser le contenu du fichier commandes.json
+
+## Etape 2
+### Afficher les commandes dans une liste en manipulant le DOM en JavaScript ou en JQuery.
+<ul>
+<li> num: 123 - client John </li>
+<li> num: 124 - client Jean </li>
+</ul>
+
+## Exercice Restaurants (bonus)
+
+<ul>
+<li>Créer une page HTML vide</li>
+<li>Utiliser Ajax (JS ou Jquery) pour charger le fichier restaurants.xml</li>
+<li>Créer a ul. Pour chaque <restaurant>, créer un li avec le nom & l'adresse du restaurant. Vous devez changer la
+couleur du texte par rapport au type du restaurant ("sitdown" ou "bar").</li>
+</ul>
---
+++ ajax/index.js
@@ -0,0 +1,55 @@
+function afficherContentXml(xmlDocument){
+    let contentDiv = "<ul> ";
+    let restaurants = xmlDocument.getElementsByTagName("restaurant");
+
+    for(let i = 0; i < restaurants.length; i++){
+        console.log(restaurants.item(i).attributes.getNamedItem("name"));
+        console.log(restaurants.item(i).attributes.getNamedItem("name").value);
+        contentDiv += "<li>nom "+restaurants.item(i).attributes.getNamedItem("name").value+" ";
+        contentDiv += "adresse: "+restaurants.item(i).attributes.getNamedItem("address").value+" </li>";
+    }
+
+    contentDiv += "</ul>";
+
+    let target = document.getElementById("restaurants");
+    target.innerHTML = contentDiv;
+}
+
+function getXMLHttpRequest() {
+    let xhr = null;
+
+    if (window.XMLHttpRequest || window.ActiveXObject) {
+        if (window.ActiveXObject) {
+            try {
+                xhr = new ActiveXObject("Msxml2.XMLHTTP");
+            } catch(e) {
+                xhr = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+        } else {
+            xhr = new XMLHttpRequest();
+        }
+    } else {
+        alert("Votre navigateur ne supporte pas l'objet XMLHTTPRequest...");
+        return null;
+    }
+
+    return xhr;
+}
+
+function request() {
+    let xhr = getXMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && (xhr.status === 200 || xhr.status === 0)) {
+            afficherContentXml(this.responseXML);
+        }
+    };
+
+    xhr.open("GET", "restaurants.xml?rand="+Math.random(), true);
+    xhr.setRequestHeader("Content-Type", "text/xml");
+    xhr.send(null);
+}
+
+function chargerFichier() {
+    request();
+}
